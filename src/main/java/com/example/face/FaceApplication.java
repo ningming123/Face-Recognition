f8c882481609c844c5216463678aa5b41fad4ef7
ningming123 <948491705@qq.com>
2020-09-04 13:03:20 +0800

    整合mybatis

--- src/main/java/com/example/face/FaceApplication.java
+++ src/main/java/com/example/face/FaceApplication.java
@@ -1,10 +1,13 @@
 package com.example.face;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = {"com.example.face"})
+@MapperScan("com.example.face.sys.mapper")
 public class FaceApplication {
 
     public static void main(String[] args) {
