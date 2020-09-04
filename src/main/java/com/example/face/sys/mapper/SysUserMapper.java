a647a00458b22c48f31c23c68dfaacc6a2eeb7f2
ningming123 <948491705@qq.com>
2020-09-04 13:03:43 +0800

    整合mybatis

---
+++ src/main/java/com/example/face/sys/mapper/SysUserMapper.java
@@ -0,0 +1,8 @@
+package com.example.face.sys.mapper;
+
+import com.example.face.sys.model.SysUser;
+
+public interface SysUserMapper {
+
+    SysUser getUser();
+}
---
+++ src/main/java/com/example/face/sys/model/SysUser.java
@@ -0,0 +1,9 @@
+package com.example.face.sys.model;
+
+import lombok.Data;
+
+@Data
+public class SysUser {
+
+    private String id;
+}
